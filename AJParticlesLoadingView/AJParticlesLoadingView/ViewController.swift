c3145ca50188b5380b31f7e2de88ece1fde54c4e
Arpit Jain <arpit.jain@indianic.com>
2017-12-25 17:26:01 +0530

    Minor Changes

--- AJParticlesLoadingView/AJParticlesLoadingView/ViewController.swift
+++ AJParticlesLoadingView/AJParticlesLoadingView/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
          */
         
         // Set the Particle Image
-        objAJParticlesLoadingView.particleImage = .balloon
+        objAJParticlesLoadingView.particleImage = .heart
         
         // Set the Particle Effect Type
         objAJParticlesLoadingView.particleEffect = .fireflies
